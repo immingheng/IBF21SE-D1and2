b9fcfaf058d9a4d621fdabad044714ccca57d7d5
Lim Ming Heng <im.mingheng@gmail.com>
2021-12-03 12:03:57 +0800

    Empty Maven Project

---
+++ day1/src/main/java/ibf2021/cart.java
@@ -0,0 +1,15 @@
+/* This code is written for Software Engineering Fundamentals (Java) Workshop 1 
+Written by: Lim Ming Heng
+Dated: 3/12/2021
+*/
+
+package ibf2021;
+
+
+public class cart {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
